3e78a0b1ccefea54181afcaf5cb31e6b475f9dad
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2024-04-30 19:56:35 -0300

    ajustes

--- app.py
+++ app.py
@@ -27,7 +27,10 @@ senha.send_keys('contabilidade123456')
 botao_entrar = driver.find_element(By.XPATH,"//button[@id='Entrar']")
 sleep(2)
 botao_entrar.click()
+sleep(3)
+    
 # 5.1 - Extrair dados da planilha
+
 empresas = openpyxl.load_workbook('./empresas.xlsx')
 pagina_empresas = empresas['dados empresas']
 
@@ -36,29 +39,33 @@ for linha in pagina_empresas.iter_rows(min_row=2,values_only=True):
 
 # 5.2 - Preencher cada campo
 
-driver.find_element(By.ID, 'nomeEmpresa').send_keys(nome_empresa)
-sleep(1)
+    driver.find_element(By.ID, 'nomeEmpresa').send_keys(nome_empresa)
+    sleep(1)
 
-driver.find_element(By.ID,'emailEmpresa').send_keys(email)
-sleep(1)
+    driver.find_element(By.ID,'emailEmpresa').send_keys(email)
+    sleep(1)
 
-driver.find_element(By.ID,'telefoneEmpresa').send_keys(telefone)
-sleep(1)
+    driver.find_element(By.ID,'telefoneEmpresa').send_keys(telefone)
+    sleep(1)
 
-driver.find_element(By.ID,'enderecoEmpresa').send_keys(endereco)
-sleep(1)
+    driver.find_element(By.ID,'enderecoEmpresa').send_keys(endereco)
+    sleep(1)
 
-driver.find_element(By.ID,'cnpj').send_keys(cnpj)
-sleep(1)
+    driver.find_element(By.ID,'cnpj').send_keys(cnpj)
+    sleep(1)
 
-driver.find_element(By.ID,'areaAtuacao').send_keys(area_atuacao)
-sleep(1)
+    driver.find_element(By.ID,'areaAtuacao').send_keys(area_atuacao)
+    sleep(1)
 
-driver.find_element(By.ID, 'numeroFuncionarios').send_keys(quantidade_de_funcionarios)
-sleep(1)
+    driver.find_element(By.ID, 'numeroFuncionarios').send_keys(quantidade_de_funcionarios)
+    sleep(1)
 
-driver.find_element(By.ID,'dataFuncionarios').send_keys(data_fundacao)
-sleep(1)
+    driver.find_element(By.ID,'dataFuncionarios').send_keys(data_fundacao)
+    sleep(1)
+
+    # 6 - cadastrar 
+    driver.find_element(By.ID,'Cadastrar').click()
+    sleep(3)
 
 
 
